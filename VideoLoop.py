a71b97538a07bf8e31a4720d443089a00cf4fd7a
Ale Cremer <ale.cremer.dev@gmail.com>
2026-02-03 16:38:21 -0300

    refactor: video loop

---
+++ VideoLoop.py
@@ -0,0 +1,36 @@
+from configs.video_inference_config import VideoInferenceConfig
+from abc import ABC, abstractmethod
+
+class VideoEngine(ABC):
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class VideoLoop:
+    def __init__(self, config: VideoInferenceConfig, video_engine: VideoEngine):
+        self.config: VideoInferenceConfig = config
+        self.video_engine: VideoEngine = video_engine
+    
+    def _handle_callback(self, callback: callable):
+        if callback is not None:
+            callback()
+
+    def _should_process(self, frame_count: int, frames_to_skip: int):
+        if frames_to_skip <= 0:
+            return True
+        return frame_count % frames_to_skip == 0
+    
+    def loop(self):
+        frame_count: int = 0
+
+        while True:
+
+            frame_count += 1
+
+            if self._should_process(frame_count, self.config.skip_frames):
+
+                self._handle_callback(self.config.loop_start_callback)
+                self.video_engine.execute()
+                self._handle_callback(self.config.loop_end_callback)
