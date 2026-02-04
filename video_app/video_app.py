f2b0bf6647f81dd763aa143246937e2a33c078ae
Ale Cremer <ale.cremer.dev@gmail.com>
2026-02-03 22:07:16 -0300

    chore: inference runner in app video

--- video_app/video_app.py
+++ video_app/video_app.py
@@ -1,7 +1,7 @@
 from video_app.video_loop import VideoLoop
 from configs.video_inference_config import VideoInferenceConfig
 from video_executors.opencv_video_executor import OpenCV_VideoExecutor
-from inference_pipeline.YOLO_inference_pipeline import YOLO_InferencePipeline
+from inference_runners.inference_runner import InferenceRunner
 from video_app.source.video_source_config import VideoSourceConfig
 from video_app.video_config_converter import VideoConfigConverter
 from rendering.opencv_drawer import OpenCV_Drawer
@@ -12,7 +12,7 @@ class VideoApp:
         
         video_source_config = VideoConfigConverter.video_inference_to_source_config(config)
 
-        inference_pipeline = YOLO_InferencePipeline() #TODO: Add SETR-PUP Switcher (pipeline factory)
+        inference_pipeline = InferenceRunner()
         renderer = OpenCV_Drawer()
         executor = OpenCV_VideoExecutor(config, inference_pipeline, video_source_config, renderer)
         self.looper = VideoLoop(config, executor)
