0926f8e4aa0a9249c59c2b50ae93aaaccc7637b6
Ale Cremer <ale.cremer.dev@gmail.com>
2026-02-11 15:40:38 -0300

    refactor: keyboard handler using action dispatcher

--- annotation_transition/renderer/keyboard_handler.py
+++ annotation_transition/renderer/keyboard_handler.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import List
 from annotation_transition.renderer.action_dispatcher import ActionDispatcher
-
+from annotation_action import AnnotationAction
 @dataclass
 class KeyCommand:
     key: str
@@ -10,20 +10,32 @@ class KeyCommand:
 class KeyboardHandler:
 
     def __init__(self, action_dispatcher: ActionDispatcher):
-        self.annotation_engine = action_dispatcher
+        self.action_dispatcher = action_dispatcher
         self.commands = List[KeyCommand]
 
     def build(self):
         cmds: List[KeyCommand] = []
-        cmds.append(KeyCommand('g', self.annotation_engine.next_img))
-        cmds.append(KeyCommand('d', self.annotation_engine.previous_img))
-        cmds.append(KeyCommand('t', self.annotation_engine.reset_annotation_cell))
-        cmds.append(KeyCommand('e', self.annotation_engine.toggle_show_ui))
-        cmds.append(KeyCommand('s', self.annotation_engine.save_annotations))
-        cmds.append(KeyCommand('w', self.annotation_engine.undo_polygon_point))
-        cmds.append(KeyCommand('f', self.annotation_engine.done_or_create_polygon))
-        cmds.append(KeyCommand('q', self.annotation_engine.quit))
-        cmds.append(KeyCommand('r', self.annotation_engine.set_create_rectangle))
+        cmds.append(KeyCommand('g', self.action_dispatcher.dispatch(AnnotationAction.NEXT_IMG)))
+        cmds.append(KeyCommand('d', self.action_dispatcher.dispatch(AnnotationAction.PREVIOUS_IMG)))
+        cmds.append(KeyCommand('t', self.action_dispatcher.dispatch(AnnotationAction.RESET_ANNOTATION_CELL)))
+        cmds.append(KeyCommand('e', self.action_dispatcher.dispatch(AnnotationAction.TOGGLE_SHOW_UI)))
+        cmds.append(KeyCommand('s', self.action_dispatcher.dispatch(AnnotationAction.SAVE_ANNOTATIONS)))
+        cmds.append(KeyCommand('w', self.action_dispatcher.dispatch(AnnotationAction.UNDO_MASK_POINT)))
+        cmds.append(KeyCommand('f', self.action_dispatcher.dispatch(AnnotationAction.DONE_MASK)))
+        cmds.append(KeyCommand('q', self.action_dispatcher.dispatch(AnnotationAction.QUIT)))
+        cmds.append(KeyCommand('r', self.action_dispatcher.dispatch(AnnotationAction.CREATE_BOX)))
+
+       
+        # cmds: List[KeyCommand] = []
+        # cmds.append(KeyCommand('g', self.annotation_engine.next_img))
+        # cmds.append(KeyCommand('d', self.annotation_engine.previous_img))
+        # cmds.append(KeyCommand('t', self.annotation_engine.reset_annotation_cell))
+        # cmds.append(KeyCommand('e', self.annotation_engine.toggle_show_ui))
+        # cmds.append(KeyCommand('s', self.annotation_engine.save_annotations))
+        # cmds.append(KeyCommand('w', self.annotation_engine.undo_polygon_point))
+        # cmds.append(KeyCommand('f', self.annotation_engine.done_or_create_polygon))
+        # cmds.append(KeyCommand('q', self.annotation_engine.quit))
+        # cmds.append(KeyCommand('r', self.annotation_engine.set_create_rectangle))
 
         self.commands = cmds
 
--- annotation_transition/renderer/opencv_pipeline.py
+++ annotation_transition/renderer/opencv_pipeline.py
@@ -5,11 +5,19 @@ from keyboard_handler import KeyboardHandler
 class OpencvPipeline:
 
     def __init__(self, mouse_handler: MouseHandler, keyboard_handler: KeyboardHandler):
-        window_name = "Annotation"
+        self.window_name = "Annotation"
         self.input_context = InputContext()
         self.keyboard_handler = keyboard_handler
+        self.keyboard_handler.build()
 
-        cv2.namedWindow(window_name)
-        cv2.setMouseCallback(window_name, mouse_handler.mouse_callback, param=self.input_context)
+        cv2.namedWindow(self.window_name)
+        cv2.setMouseCallback(self.window_name, mouse_handler.mouse_callback, param=self.input_context)
 
+    def routine(self):
+
+        key = cv2.waitKey(10) & 0xFF  
+        self.keyboard_handler.routine(key)
         
+        if cv2.getWindowProperty(self.window_name, cv2.WND_PROP_VISIBLE) < 1:
+            print("killing...")
+            #TODO: kill loop here
