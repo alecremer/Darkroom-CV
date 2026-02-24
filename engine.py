af3a9f47e6ba899ae3ade9881b28672d30c68dad
Ale Cremer <ale.cremer.dev@gmail.com>
2026-02-24 16:13:00 -0300

    chore: engine cleanup

--- engine.py
+++ engine.py
@@ -16,68 +16,19 @@ from train_engine.train_engine import TrainEngine
 from video_app.video_app import VideoApp
 
 
-
-# class Train:
-
-#     @classmethod
-#     def train_one_model(self, train_cfg: TrainModelConfig):
-        
-#         # parse
-#         epochs = train_cfg.epochs
-#         path = train_cfg.dataset_path
-#         device = train_cfg.device
-#         model = train_cfg.model
-#         results_folder_name = train_cfg.results_folder_name
-
-#         model = YOLO("models/" + model)
-
-#         if train_cfg.model_par_config:
-
-#             results = model.train(data=(path + "/data.yaml"), device=device, 
-#                             project=path + "/runs", name=results_folder_name, **train_cfg.model_par_config)
-#         else:
-#             results = model.train(data=(path + "/data.yaml"), epochs=epochs, device=device, 
-#                             project=path + "/runs", name=results_folder_name, patience=50)
-#         model.val()
-
-#     @classmethod
-#     def train(self, train_cfg_list: List[TrainModelConfig]):
-
-#         if train_cfg_list and len(train_cfg_list) > 0:
-            
-#             for cfg in train_cfg_list:
-
-#                 self.train_one_model(cfg)
-
-#         else:
-#             raise ValueError("train configuration could not be empty")
-
 class Engine:
 
-    
     _train = TrainEngine()
-
-    # def __init__(self):
-    #     # logging.getLogger("ultralytics").setLevel(logging.CRITICAL)
-    #     self.create_rectangle = False
-    #     self.drawing_rectangle = False
-    #     self.show_ui = True
-    #     self.x_y_mouse = 0,0
 
     def train(self, train_cfg_list: List[TrainModelConfig]): 
         self._train.train(train_cfg_list)
-    
 
     def test(self, weight_path, test_path, show_image = True):
         model_trained = YOLO(weight_path)
         result = model_trained.predict(test_path, show=show_image)[0] 
 
     def annotate(self, img_path: str, detect_model_config: List[DetectModelConfig]):
-        # annotation_tool = AnnotationPipeline()
         annotation_tool = AnnotationOpencvFactory()
-        # weight_path = [config.weights_path for config in detect_model_config]
-        # models_trained = self._set_trained_models(weight_path)
-        # models_trained = None
         annotation_tool.annotate(img_path, detect_model_config)
 
     def live(self, video_config: VideoInferenceConfig, config_path: str):
